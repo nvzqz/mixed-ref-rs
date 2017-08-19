25e334af93313fb9cde9435f58b9c79cd2a13eb1
Nikolai Vazquez <nvazquez1297@gmail.com>
2017-08-19 03:41:03 -0400

    Create MixedRefMut type

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
-use std::ops::Deref;
-use std::borrow::Borrow;
+use std::ops::{Deref, DerefMut};
+use std::borrow::{Borrow, BorrowMut};
 
 /// A reference to either owned or borrowed data.
 ///
 /// If the data is owned, it's recommended to provide the `'static` lifetime.
 /// In some cases, there's no other option.
+///
+/// If mutably borrowing data, use [`MixedRefMut`].
+///
+/// [`MixedRefMut`]: enum.MixedRefMut.html
 pub enum MixedRef<'a, T: ?Sized + 'a> {
     /// Owned, boxed data.
     Owned(Box<T>),
@@ -12,9 +16,27 @@ pub enum MixedRef<'a, T: ?Sized + 'a> {
     Borrowed(&'a T)
 }
 
+/// A reference to either owned or mutably borrowed data.
+///
+/// This acts similarly to [`MixedRef`], except that the inner data is mutable.
+///
+/// [`MixedRef`]: enum.MixedRef.html
+pub enum MixedRefMut<'a, T: ?Sized + 'a> {
+    /// Owned, boxed data.
+    Owned(Box<T>),
+    /// Borrowed, mutable data.
+    Borrowed(&'a mut T)
+}
+
 impl<'a, T: Default> Default for MixedRef<'a, T> {
     fn default() -> Self {
         MixedRef::Owned(Default::default())
+    }
+}
+
+impl<'a, T: Default> Default for MixedRefMut<'a, T> {
+    fn default() -> Self {
+        MixedRefMut::Owned(Default::default())
     }
 }
 
@@ -24,9 +46,21 @@ impl<'a, T: ?Sized> From<&'a T> for MixedRef<'a, T> {
     }
 }
 
+impl<'a, T: ?Sized> From<&'a mut T> for MixedRefMut<'a, T> {
+    fn from(r: &'a mut T) -> Self {
+        MixedRefMut::Borrowed(r)
+    }
+}
+
 impl<'a, T: ?Sized> From<Box<T>> for MixedRef<'a, T> {
     fn from(b: Box<T>) -> Self {
         MixedRef::Owned(b)
+    }
+}
+
+impl<'a, T: ?Sized> From<Box<T>> for MixedRefMut<'a, T> {
+    fn from(b: Box<T>) -> Self {
+        MixedRefMut::Owned(b)
     }
 }
 
@@ -41,14 +75,58 @@ impl<'a, T: ?Sized> Deref for MixedRef<'a, T> {
     }
 }
 
+impl<'a, T: ?Sized> Deref for MixedRefMut<'a, T> {
+    type Target = T;
+
+    fn deref(&self) -> &T {
+        match *self {
+            MixedRefMut::Owned(ref b) => b,
+            MixedRefMut::Borrowed(ref r) => r
+        }
+    }
+}
+
+impl<'a, T: ?Sized> DerefMut for MixedRefMut<'a, T> {
+    fn deref_mut (&mut self) -> &mut T {
+        match *self {
+            MixedRefMut::Owned(ref mut b) => b,
+            MixedRefMut::Borrowed(ref mut r) => r
+        }
+    }
+}
+
 impl<'a, T: ?Sized> AsRef<T> for MixedRef<'a, T> {
     fn as_ref(&self) -> &T {
         self
     }
 }
 
+impl<'a, T: ?Sized> AsRef<T> for MixedRefMut<'a, T> {
+    fn as_ref(&self) -> &T {
+        self
+    }
+}
+
+impl<'a, T: ?Sized> AsMut<T> for MixedRefMut<'a, T> {
+    fn as_mut(&mut self) -> &mut T {
+        self
+    }
+}
+
 impl<'a, T: ?Sized> Borrow<T> for MixedRef<'a, T> {
     fn borrow(&self) -> &T {
+        self
+    }
+}
+
+impl<'a, T: ?Sized> Borrow<T> for MixedRefMut<'a, T> {
+    fn borrow(&self) -> &T {
+        self
+    }
+}
+
+impl<'a, T: ?Sized> BorrowMut<T> for MixedRefMut<'a, T> {
+    fn borrow_mut(&mut self) -> &mut T {
         self
     }
 }
